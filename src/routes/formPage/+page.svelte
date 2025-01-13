a9689540ce8b6d580b7792c8249d0d11282fb09b
GregoryDionisi <gregoryd324@gmail.com>
2025-01-13 11:50:12 +0100

    Versione finale

--- src/routes/formPage/+page.svelte
+++ src/routes/formPage/+page.svelte
@@ -3,49 +3,67 @@
 
     //let name = $state(); //variabile reattiva che reagisce ai cambiamenti della UI
 
-    let formState = $state({
+   /* let formState = $state({
         name: "",
         birthday: "",
         step: 0,
         error: "",
+    }); */
+
+    let formState = $state({
+        answers: {},
+        step: 0,
+        error: "",
     });
+
+    const QUESTION = [
+        {
+            id: "name",
+            question: "What is your name",
+            type: "text"
+        },
+        {
+            id: "birthday",
+            question: "What is your birthday",
+            type: "date"
+        },
+        {
+            id: "color",
+            question: "What is your favourite color",
+            type: "color"
+        }
+    ]
+
+    function nextStep(id){
+        if (formState.id !== ""){
+            formState.step += 1;
+            formState.error = "";
+        } else {
+            formState.error = "Please enter your birthday";
+        }
+    }
 </script>
 
 <h1>Form Page</h1>
 
-<Header name = {formState.name}/>
+<Header name = {formState.answers?.name}/> <!--si mette punto di domanda perchè la proprietà name non c'è ancora-->
 
 <p>Step: {formState.step + 1}</p> <!--Per un utente sarebbe meglio vedere 1 invece di 0-->
 
+{#each QUESTION as {id, question, type}}
+    {@render formStep({id, question, type})}
+{/each}
 
-{#if formState.step === 0}
-    <div>
-        <label for = "name">Your name</label>
-        <input type = "text" bind:value = {formState.name} /> <!--per collegare l'input a una variabile usiamo bind-->
-    </div>
-    <button onclick = {() => {
-        if (formState.name !== ""){
-            formState.step += 1;
-            formState.error = "";
-        } else {
-            formState.error = "Please enter your name";
-        }
-    }}>Next</button>
-{:else if formState.step === 1}
-    <div>
-        <label for = "birthday">Your birthday</label>
-        <input type = "date" bind:value = {formState.birthday} /> <!--per collegare l'input a una variabile usiamo bind-->
-    </div>
-        <button onclick = {() => {
-            if (formState.birthday !== ""){
-                formState.step += 1;
-                formState.error = "";
-            } else {
-                formState.error = "Please enter your birthday";
-            }
-        }}>Next</button>
-{/if}
+{JSON.stringify(formState)}
 
 {#if formState.error}
     <p>{formState.error}</p>
 {/if}
+
+{#snippet formStep({id, question, type})} <!--Snippet è una sorta di funzione-->
+    <div>
+        <label for={id}>{question}</label>
+        <input {type} bind:value = {formState.answers[id]}> <!--metti bind attaccato a value-->
+    </div>
+    <!-- <button onlick={nextStep()}>Next</button> -->
+{/snippet}
