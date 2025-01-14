c8bba5a42a8d3e1258bf054847cbaff160e6111c
GregoryDionisi <gregoryd324@gmail.com>
2025-01-14 10:47:40 +0100

    Versione finale

--- src/routes/formPage/+page.svelte
+++ src/routes/formPage/+page.svelte
@@ -1,69 +1,81 @@
 <script>
     import Header from "$lib/Header.svelte";
-
+ 
     //let name = $state(); //variabile reattiva che reagisce ai cambiamenti della UI
-
-   /* let formState = $state({
-        name: "",
-        birthday: "",
-        step: 0,
-        error: "",
-    }); */
-
+ 
     let formState = $state({
         answers: {},
         step: 0,
         error: "",
     });
-
+ 
     const QUESTION = [
         {
             id: "name",
-            question: "What is your name",
-            type: "text"
+            question: "What is your name?",
+            type:"text"
         },
         {
             id: "birthday",
-            question: "What is your birthday",
-            type: "date"
+            question: "What is your birthday?",
+            type:"date"
         },
         {
             id: "color",
-            question: "What is your favourite color",
+            question: "What is your favourite color?",
             type: "color"
         }
     ]
-
-    function nextStep(id){
-        if (formState.id !== ""){
-            formState.step += 1;
-            formState.error = "";
-        } else {
-            formState.error = "Please enter your birthday";
-        }
+   
+    function nextStep(id) {
+ 
+        if (formState.answers[id]){
+                formState.step += 1;
+                formState.error = "";
+            } else {
+                formState.error = `Please enter your ${id}`;
+            }
     }
+   
 </script>
-
+ 
 <h1>Form Page</h1>
+ 
+<Header name = {formState.answers?.name}/> <!--? -> se c'è lo metto senno niente-->
 
-<Header name = {formState.answers?.name}/> <!--si mette punto di domanda perchè la proprietà name non c'è ancora-->
-
-<p>Step: {formState.step + 1}</p> <!--Per un utente sarebbe meglio vedere 1 invece di 0-->
-
-{#each QUESTION as {id, question, type}}
-    {@render formStep({id, question, type})}
+{#if formState.step >= QUESTION.length}
+    <p>Thank you for your {formState.answers.name} answers!</p>
+{:else}
+    <p>Step: {formState.step + 1}</p> <!--Per un utente sarebbe meglio vedere 1 invece di 0-->
+ {/if}
+ 
+{#each QUESTION as {id, question,type}, bob} <!--bob è il contatore che conterà ogni singola iterazione-->
+    {#if formState.step === bob}
+        {@render formStep({id, question, type})}
+    {/if}
 {/each}
-
-{JSON.stringify(formState)}
-
+ 
+<!-- {JSON.stringify(formState)} -->
+ 
+   
+         <!--<button onclick = {() => {
+            if (formState.birthday !== ""){
+                formState.step += 1;
+                formState.error = "";
+            } else {
+                formState.error = "Please enter your birthday";
+            }
+        }}>Next</button> -->
+ 
+ 
 {#if formState.error}
     <p>{formState.error}</p>
 {/if}
-
-{#snippet formStep({id, question, type})} <!--Snippet è una sorta di funzione-->
-    <div>
-        <label for={id}>{question}</label>
-        <input {type} bind:value = {formState.answers[id]}> <!--metti bind attaccato a value-->
-    </div>
-    <!-- <button onlick={nextStep()}>Next</button> -->
+ 
+{#snippet formStep ({id,question,type})}
+<div>
+    <label for = {id}>{question} </label>
+    <input {type} bind:value = {formState.answers[id]} /> <!--per collegare l'input a una variabile usiamo bind-->
+</div>
+<button onclick={() => nextStep(id)}>Next</button> <!--quando richiamo una funzione con un parametro uso l'arrow function -->
 {/snippet}
