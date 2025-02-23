6bf3223d8335ad82c6833157007ba5f5d5d391d9
GregoryDionisi <gregoryd324@gmail.com>
2025-02-23 13:03:41 +0100

    Aggiunta alternativa a map

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -3,9 +3,27 @@ export const load = async () => {
     const posts = await response.json();
 
     return {
-        posts: posts.map(post => ({
+        posts: posts.map(post => ({ //ricordati che stai ritornando una proprietà contenente una serie di oggetti e NON un'oggetto direttamente
             id: post.id,
             title: post.title
         }))
     };
 };
+
+
+// Alternativa 2: usando forEach
+/* export const load = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const fullPosts = await response.json();
+    
+    const simplifiedPosts = [];
+    fullPosts.forEach(post => {
+        simplifiedPosts.push({
+            id: post.id,
+            title: post.title
+        });
+    });
+
+    return { posts: simplifiedPosts };
+};
+ */
